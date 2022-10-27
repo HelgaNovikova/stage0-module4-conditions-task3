53507caf9e083b749b3bcf239da379506974980f
HelgaNovikova <helpotaki@rambler.ru>
2022-10-27 16:56:47 -0400

    task4.3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String allVowels = "eyuioa";
+        String allConsonant = "qwrtpsdfghjklzxcvbnm";
+        if (allVowels.indexOf(Character.toLowerCase(character)) != -1){
+            System.out.println("Vowel");
+        }
+        else if (allConsonant.indexOf(Character.toLowerCase(character)) != -1){
+            System.out.println("Consonant");
+        }
+        else System.out.println("wrong alphabet!");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        String allLetters = "qwertyuiopasdfghjklzxcvbnm";
+        if (allLetters.indexOf(Character.toLowerCase(symbol)) != -1){
+            System.out.println("English");
+        }
+        else {
+            System.out.println("Non English");
+        }
     }
 }
